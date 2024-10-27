25293ffc55910a221bfa7689af45ce99f82f8ba6
Abreham Gebremedhin <abreham.gmedhin12@gmail.com>
2024-10-27 22:51:15 +0300

    added local

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(xss());
 app.use(hpp());
 
 // Specify your frontend origin
-const allowedOrigins = ['https://auth-frontend-ns448v832-abrehamgebremedhins-projects.vercel.app', 'https://radiant-shortbread-9fe958.netlify.app'];
+const allowedOrigins = ['https://auth-frontend-ns448v832-abrehamgebremedhins-projects.vercel.app', 'https://radiant-shortbread-9fe958.netlify.app', 'http://localhost:3000'];
 
 app.use(cors({
     origin: allowedOrigins,
